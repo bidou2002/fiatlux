56971ca49fbbbef9d797bf1ee2c854688b468c51
pjanin-potiron <pierre.janin-potiron@lam.fr>
2023-10-25 15:56:19 +0200

    Restore stash

--- fiatlux/source_dev.py
+++ fiatlux/source_dev.py
@@ -4,25 +4,19 @@ from enum import auto, Enum
 
 import numpy as np
 
-from .spectrum_dev import Spectrum, SpectrumType
-
-
-class SourceType(Enum):
-    POINT_SOURCE = auto()
-    EXTENDED_SOURCE = auto()
+from .spectrum_dev import Spectrum, SpectralType, Monochromatic
 
 
 @dataclass
 class Source:
-    type: SourceType = SourceType.POINT_SOURCE
-    spectrum: Spectrum = Spectrum(type=SpectrumType.MONOCHROMATIC)
+    spectrum: Spectrum = Monochromatic(wavelength=550e-9, irradiance=1)
 
-    def __post_init__(self):
-        match self.spectrum:
-            case SpectrumType.MONOCHROMATIC:
-                pass
-            case SpectrumType.PHOTOMETRIC:
-                pass
-            case SpectrumType.BLACK_BODY:
-                pass
-            
+
+@dataclass(kw_only=True)
+class PointSource:
+    pass
+
+
+@dataclass(kw_only=True)
+class ExtendedSource:
+    pass
--- fiatlux/spectrum_dev.py
+++ fiatlux/spectrum_dev.py
@@ -4,33 +4,89 @@ from enum import auto, Enum
 
 import numpy as np
 
-
-class SpectrumType(Enum):
-    MONOCHROMATIC = auto()
-    PHOTOMETRIC = auto()
-    BLACK_BODY = auto()
+import astropy.constants as C
+import astropy.units as u
 
 
-class Monochromatic:
-    pass
+class SpectralBand(Enum):
+    # spectral bands with caracteristics λeff, Δλ and f_0
+    U = [0.360e-6, 0.070e-6, 2.0e12]
+    B = [0.500e-6, 0.090e-6, 3.3e12]
+    V = [0.550e-6, 0.090e-6, 3.3e12]
+    R = [0.640e-6, 0.150e-6, 4.0e12]
 
 
-class BlackBody:
-    pass
-
-
-class Photometric:
-    pass
+class SpectralFilter(Enum):
+    U = [360e-9, 550e-9]
+    B = [360e-9, 550e-9]
 
 
 @dataclass
 class Spectrum:
-    type: SpectrumType
-    match type:
-        case SpectrumType.MONOCHROMATIC:
-            print("Bite")
-        case SpectrumType.PHOTOMETRIC:
-            print("cul")
-        case SpectrumType.BLACK_BODY:
-            print("couille")
-    photon_number: float = None
+    _photon_number: float = 0.0
+
+    # def __post_init__(self):
+    #     # need typing ???
+    #     self._is_filtered: bool = False
+    #     self._photon_number: float = 0.0
+    @abstractmethod
+    def compute_photon_flux(self, **kwargs):
+        pass
+
+
+@dataclass(kw_only=True)
+class Monochromatic(Spectrum):
+    """
+    A monochromatic radiation at wavelength λ with irradiance I
+    """
+
+    wavelength: float
+    irradiance: float
+
+    def compute_photon_flux(self):
+        print(self._photon_number)
+        c = C.c  # speed of light
+        h = C.h  # Plank's constant
+        self._photon_number = self.irradiance / ((h * c) / self.wavelength)
+
+
+@dataclass(kw_only=True)
+class BlackBody(Spectrum):
+    """
+    A black body at temperature T.
+    """
+
+    temperature: float
+    filter: SpectralFilter
+
+    def compute_photon_flux(self, filter: SpectralFilter):
+        # define constants
+        c = C.c  # speed of light
+        h = C.h  # Plank's constant
+        k_B = C.k_B  # Boltzman's constant
+
+        c1 = 2 * h * c**2
+        c2 = h * c / k_B
+        nu = 10
+        I = 0
+        for n in range(1, 10):
+            I += (
+                (c1 / k_B**4)
+                * (np.exp(-n * k_B.value * nu) / (n**4))
+                * ((n * k_B * nu) + 3 * (n * k_B * nu) ** 2 + 6 * (n * k_B * nu) + 6)
+            )
+            print(I)
+
+
+@dataclass(kw_only=True)
+class Photometric(Spectrum):
+    """
+    A spectrally filtered radiation with with magnitude m and spectral band B.
+    """
+
+    magnitude: float
+    spectral_band: SpectralBand
+
+    def compute_photon_flux(self):
+        lambda_eff, delta_lambda, f_0 = self.spectral_band.value
+        return (f_0 / 368) * 10 ** (-self.magnitude / 2.5)
