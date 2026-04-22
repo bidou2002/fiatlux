a1efc2b9926a0f41b7fb75594b688979c5c4071c
Pierre Janin-Potiron <pierre.janinpotiron@gmail.com>
2026-04-22 16:36:23 +0200

    Ajout des modèles de dispersion ADC et des détecteurs dans le module optique

---
+++ fiatlux/optics/__init__.py
@@ -0,0 +1,11 @@
+from .adc import ADCDispersionModel
+from .detector import Detector
+from .propagator import Propagator
+
+from .elements import *
+
+__all__ = [
+    "ADC",
+    "Detector",
+    "Propagator",
+]
